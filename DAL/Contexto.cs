2402f907b8c5ac92ecca118670740e2cbaa88a05
Yohualkis <Yohualkis99@gmail.com>
2024-09-23 21:09:52 -0400

    Se creo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YohualkisTejada_AP1_P1.Models;
+
+namespace YohualkisTejada_AP1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) 
+        : base(options) { }
+
+    public DbSet<Registros> Registros { get; set; }
+}
