9cf95cdaa4e09999b75ef1db8af7ca5237a651ca
Yohualkis <Yohualkis99@gmail.com>
2024-09-23 21:09:39 -0400

    Se creo el modelo Registros

---
+++ Models/Registros.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YohualkisTejada_AP1_P1.Models;
+
+public class Registros
+{
+    [Key]
+    public int RegistroId { get; set; }
+
+    public string? Nombre { get; set; }
+}
