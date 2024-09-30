1a904baf0d1fc2572f6b1c66a6bb7ba6476d9b31
Yohualkis <Yohualkis99@gmail.com>
2024-09-30 18:31:01 -0400

    Se creo el modelo Prestamos

---
+++ Models/Prestamos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YohualkisTejada_AP1_P1.Models;
+
+public class Prestamos
+{
+	[Key]
+	public int PrestamoId { get; set; }
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public string? Deudor { get; set; }
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public string? Concepto { get; set; }
+
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	[RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo puede alojar números.")]
+}
