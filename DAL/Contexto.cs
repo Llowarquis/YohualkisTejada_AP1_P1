432955f3eb77574057d299fd0ecde5fbd06e0f77
Yohualkis <Yohualkis99@gmail.com>
2024-10-11 12:39:59 -0400

    Se agrego el onModelCreating y el modelo "Deudores"

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,7 +8,19 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) 
         : base(options) { }
 
-    public DbSet<Registros> Registros { get; set; }
     public DbSet<Prestamos> Prestamos { get; set; }
+
+    public DbSet<Deudores> Deudores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Deudores>().HasData(new List<Deudores>()
+        {
+            new Deudores() { DeudorId=1, Nombres="Manny Machado"},
+            new Deudores() { DeudorId=2, Nombres="LeBron James"},
+            new Deudores() { DeudorId=3, Nombres="Bronny James"}
+        });
+    }
 
 }
