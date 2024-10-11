26d46f9d3a62230bbdffad891a8e024d74cc871d
Yohualkis <Yohualkis99@gmail.com>
2024-10-11 12:40:44 -0400

    Se creo el modelo Deudores

---
+++ Models/Deudores.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YohualkisTejada_AP1_P1.Models;
+
+public class Deudores
+{
+    [Key]
+    public int DeudorId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+
+    public string? Nombres { get; set; }
+}
