83037651a97a96962a873d37aa9c5a868b8c0f4a
Yohualkis <Yohualkis99@gmail.com>
2024-10-11 12:41:43 -0400

    Se cambio el nombre del atributo "Deudor" a "Nombres" y se agrego una FK y una instancia de la clase Deudores

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace YohualkisTejada_AP1_P1.Models;
 
@@ -9,7 +10,7 @@ public class Prestamos
 
 	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
 	[Required(ErrorMessage = "Este campo es obligatorio.")]
-	public string? Deudor { get; set; }
+	public string? Nombres { get; set; }
 
 	[Required(ErrorMessage = "Este campo es obligatorio.")]
 	public string? Concepto { get; set; }
@@ -17,4 +18,9 @@ public class Prestamos
 	[Required(ErrorMessage = "Este campo es obligatorio.")]
 	[RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo puede alojar números.")]
 	public double Monto { get; set; }
+
+	[ForeignKey("DeudorId")]
+	public int DeudorId { get; set; }
+
+	public Deudores? Deudor { get; set; }
 }
