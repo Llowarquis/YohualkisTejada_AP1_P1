9606532be50d6c9280914270799c9ab33ea90aab
Yohualkis <Yohualkis99@gmail.com>
2024-10-11 12:43:05 -0400

    Se creo el service de la clase Deudores

---
+++ Services/DeudoresService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using YohualkisTejada_AP1_P1.DAL;
+using YohualkisTejada_AP1_P1.Models;
+
+namespace YohualkisTejada_AP1_P1.Services;
+
+public class DeudoresService(Contexto contexto)
+{
+    private readonly Contexto _contexto = contexto;
+
+    public async Task<List<Deudores>> Listar(Expression<Func<Deudores, bool>> criterio)
+    {
+        return await _contexto.Deudores
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
